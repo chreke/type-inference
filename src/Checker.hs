0af0f03a8a5452477839b3f1a4503ef54d7a4b48
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2024-11-14 19:20:52 +0100

    Add typechecking for conditionals

--- src/Checker.hs
+++ src/Checker.hs
@@ -8,8 +8,15 @@ data Type
 data Term
   = IntLiteral
   | BoolLiteral
+  | Conditional (Term, Term, Term)
   deriving (Eq, Show)
 
 infer :: Term -> Maybe Type
 infer IntLiteral = Just Int
 infer BoolLiteral = Just Bool
+infer (Conditional (cond, term1, term2)) =
+  if infer cond == Just Bool
+    then case (infer term1, infer term2) of
+      (Just t1, Just t2) | t1 == t2 -> Just t1
+      _ -> Nothing
+    else Nothing
--- tests/Main.hs
+++ tests/Main.hs
@@ -11,3 +11,10 @@ main = hspec $ do
         infer BoolLiteral `shouldBe` Just Bool
       it "should check int literals" $
         infer IntLiteral `shouldBe` Just Int
+
+  describe "Conditionals" $
+    do
+      it "should accept a correct conditional" $
+        infer (Conditional (BoolLiteral, IntLiteral, IntLiteral)) `shouldBe` Just Int
+      it "should reject conditional with non-bool condition" $
+        infer (Conditional (IntLiteral, IntLiteral, IntLiteral)) `shouldBe` Nothing
