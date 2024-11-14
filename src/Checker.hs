03f8208ed0a84a7400a1212b0a2aa111be7f9fe8
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2024-11-14 19:40:45 +0100

    Add support for functions

--- src/Checker.hs
+++ src/Checker.hs
@@ -1,22 +1,37 @@
 module Checker where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 data Type
   = Int
   | Bool
+  | Function (Type, Type)
   deriving (Eq, Show)
 
 data Term
   = IntLiteral
   | BoolLiteral
   | Conditional (Term, Term, Term)
+  | Lambda ((Type, Type), String, Term)
+  | Variable String
   deriving (Eq, Show)
 
-infer :: Term -> Maybe Type
-infer IntLiteral = Just Int
-infer BoolLiteral = Just Bool
-infer (Conditional (cond, term1, term2)) =
-  if infer cond == Just Bool
-    then case (infer term1, infer term2) of
+type Context = Map String Type
+
+infer :: Context -> Term -> Maybe Type
+infer _ IntLiteral = Just Int
+infer _ BoolLiteral = Just Bool
+infer ctx (Conditional (cond, term1, term2)) =
+  if infer ctx cond == Just Bool
+    then case (infer ctx term1, infer ctx term2) of
       (Just t1, Just t2) | t1 == t2 -> Just t1
       _ -> Nothing
     else Nothing
+infer ctx (Variable name) =
+  Map.lookup name ctx
+infer ctx (Lambda ((input, output), arg, body)) =
+  let ctx' = Map.insert arg input ctx in
+    if infer ctx' body == Just output
+      then Just (Function (input, output))
+      else Nothing
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,20 +1,29 @@
 module Main where
 
 import Checker
+import qualified Data.Map as Map
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   describe "Literals" $
     do
+      let ctx = Map.empty
       it "should check bool literals" $
-        infer BoolLiteral `shouldBe` Just Bool
+        infer ctx BoolLiteral `shouldBe` Just Bool
       it "should check int literals" $
-        infer IntLiteral `shouldBe` Just Int
+        infer ctx IntLiteral `shouldBe` Just Int
 
   describe "Conditionals" $
     do
+      let ctx = Map.empty
       it "should accept a correct conditional" $
-        infer (Conditional (BoolLiteral, IntLiteral, IntLiteral)) `shouldBe` Just Int
+        infer ctx (Conditional (BoolLiteral, IntLiteral, IntLiteral)) `shouldBe` Just Int
       it "should reject conditional with non-bool condition" $
-        infer (Conditional (IntLiteral, IntLiteral, IntLiteral)) `shouldBe` Nothing
+        infer ctx (Conditional (IntLiteral, IntLiteral, IntLiteral)) `shouldBe` Nothing
+
+  describe "Functions" $
+    do
+      let ctx = Map.empty
+      it "should accept a well-formed function" $
+        infer ctx (Lambda ((Bool, Int), "x", Conditional (Variable "x", IntLiteral, IntLiteral))) `shouldBe` Just (Function (Bool, Int))
