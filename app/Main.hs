4a675aa938773174e3a14315b1988b170cfacd96
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2024-11-15 11:20:09 +0100

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import qualified Data.Map as Map
+import Checker
+
+literal :: Term
+literal = IntLiteral
+
+negation :: Term
+negation = Conditional (BoolLiteral, BoolLiteral, BoolLiteral)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let ctx = Map.empty
+  let state = Checker.initState
+  print (Checker.infer state ctx literal)
+  print (Checker.infer state ctx negation)
--- src/Checker.hs
+++ src/Checker.hs
@@ -1,45 +1,89 @@
 module Checker where
 
+-- import Control.Monad.State
 import Data.Map (Map)
 import qualified Data.Map as Map
+
+newtype LVar
+  = LVar Int
+  deriving (Eq, Show, Ord)
 
 data Type
   = Int
   | Bool
   | Function (Type, Type)
+  | TVar LVar
+  | Undefined -- alternatively, "bottom" or ⊥
   deriving (Eq, Show)
 
 data Term
   = IntLiteral
   | BoolLiteral
   | Conditional (Term, Term, Term)
-  | Lambda ((Type, Type), String, Term)
+  | Lambda (String, Term)
   | Variable String
   | Application (Term, Term)
   deriving (Eq, Show)
 
 type Context = Map String Type
 
-infer :: Context -> Term -> Maybe Type
-infer _ IntLiteral = Just Int
-infer _ BoolLiteral = Just Bool
-infer ctx (Conditional (cond, term1, term2)) =
-  if infer ctx cond == Just Bool
-    then case (infer ctx term1, infer ctx term2) of
-      (Just t1, Just t2) | t1 == t2 -> Just t1
-      _ -> Nothing
-    else Nothing
-infer ctx (Variable name) =
-  Map.lookup name ctx
-infer ctx (Lambda ((t1, t2), arg, body)) =
-  let ctx' = Map.insert arg t1 ctx in
-    if infer ctx' body == Just t2
-      then Just (Function (t1, t2))
-      else Nothing
-infer ctx (Application (fn, arg)) =
-  case infer ctx fn of
-    Just (Function (t1, t2)) ->
-      if infer ctx arg == Just t1
-        then Just t2
-        else Nothing
-    _ -> Nothing
+type LVars = [(LVar, Type)]
+
+data State = State {
+  stateCounter :: Int,
+  stateLVars :: LVars
+} deriving Show
+
+initState :: State
+initState = State {
+  stateCounter = 0,
+  stateLVars = []
+}
+
+newLVar :: State -> (LVar, State)
+newLVar state =
+  (LVar (stateCounter state), state { stateCounter = stateCounter state + 1})
+
+unify :: State -> LVar -> Type -> State
+unify state lvar t  =
+  let lvars = stateLVars state
+      lvars' = (lvar, t) : lvars
+  in state { stateLVars = lvars' }
+
+infer :: State -> Context -> Term -> (Type, State)
+infer state _ IntLiteral = (Int, state)
+infer state _ BoolLiteral = (Bool, state)
+infer state ctx (Conditional (cond, term1, term2)) =
+  do
+    let (ctvar, state) = newLVar state
+    let (tvar1, state) = newLVar state
+    let (tvar2, state) = newLVar state
+    let state = unify state ctvar Bool
+    let (ct, state) = infer state ctx cond
+    let (t1, state) = infer state ctx term1
+    let (t2, state) = infer state ctx term2
+    let state = unify state ctvar ct
+    let state = unify state tvar1 t1
+    let state = unify state tvar2 t2
+    let state = unify state tvar1 (TVar tvar2)
+    (TVar tvar1, state)
+infer state ctx (Variable name) =
+  (Map.findWithDefault Undefined name ctx, state)
+infer state ctx (Lambda (arg, body)) =
+  do
+    let (tvar1, state) = newLVar state
+    let t1 = TVar tvar1
+    let ctx' = Map.insert arg t1 ctx
+    let (t2, state) = infer state ctx' body
+    (Function (t1, t2), state)
+infer state ctx (Application (fn, arg)) =
+  do
+    let (tvarf, state) = newLVar state
+    let (tvar1, state) = newLVar state
+    let (tvar2, state) = newLVar state
+    let (ft, state) = infer state ctx fn
+    let (t2, state) = infer state ctx arg
+    let state = unify state tvarf ft
+    let state = unify state tvarf (Function (TVar tvar1, TVar tvar2))
+    let state = unify state tvar1 t2
+    (TVar tvar2, state)
