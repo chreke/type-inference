e16888251c7d40791541d7e3735c499a6a831ad1
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2024-12-01 23:09:48 +0100

    Add updated version of type checker

--- src/Checker.hs
+++ src/Checker.hs
@@ -81,8 +81,18 @@ infer ctx (Application (fn, arg)) =
 
 solve :: Type -> Type -> Map LVar Type -> Either String (Map LVar Type)
 solve t1 t2 subst | t1 == t2 = Right subst
-solve (TVar v) t subst = solveVar v t subst
-solve t (TVar v) subst = solveVar v t subst
+solve (TVar v) t subst =
+  let res1 = Map.lookup v subst
+      res2 =
+        case t of
+          (TVar v') -> Map.lookup v' subst
+          _ -> Nothing
+   in case (res1, res2) of
+        (Just t', _) -> solve t' t subst
+        (_, Just t') -> solve (TVar v) t' subst
+        -- TODO: Add the occurs check
+        _ -> Right $ Map.insert v t subst
+solve t (TVar v) subst = solve (TVar v) t subst
 solve (Function (in1, out1)) (Function (in2, out2)) subst =
   solve in1 in2 subst >>= solve out1 out2
 solve t1 t2 _subst = Left (printf "Could not unify %s with %s" (show t1) (show t2))
@@ -110,9 +120,127 @@ solveVar v t subst =
    in case (res1, res2) of
         (Just t', _) -> solve t' t subst
         (_, Just t') -> solve (TVar v) t' subst
-        _ | occursCheck v t subst ->
-          Left (printf "Self-reference: %s %s" (show v) (show t))
+        _
+          | occursCheck v t subst ->
+              Left (printf "Self-reference: %s %s" (show v) (show t))
         _ -> Right $ Map.insert v t subst
+
+-- If v is bound, unify with the bound value
+-- If x is a bound variable, unify with the bound value
+-- Otherwise, add a variable mapping
+-- If   either variable is bound, get the bound variable
+--
+-- solveVar v1 (TVar v2) subst =
+-- case Map.lookup v2 subst of
+--   Just t ->
+-- solveVar v t subst =
+--   case Map.lookup v subst of
+--     Just t' -> solve t' t subst
+--     Nothing ->
+--       case t of
+--         TVar v' ->
+--           case Map.lookup v' subst of
+--             Just t' -> solve (TVar v) t' subst
+--             Nothing -> Just $ Map.insert v t subst
+--         _ -> Just $ Map.insert v t subst
+-- TODO: Add occurs check
+
+newVar :: State Int LVar
+newVar = do
+  s <- get
+  let next = s + 1
+  put s
+  return (LVar next)
+
+data LTerm
+  = LIntLiteral
+  | LBoolLiteral
+  | LConditional ((LVar, LTerm), (LVar, LTerm), (LVar, LTerm))
+  | LLambda (LVar, (LVar, LTerm))
+  | LVariable LVar
+  | LApplication ((LVar, LTerm), (LVar, LTerm))
+  | LUndefined
+  deriving (Eq, Show)
+
+type Symbols = Map String LVar
+
+label :: Symbols -> Term -> State Int LTerm
+label _ IntLiteral = pure LIntLiteral
+label _ BoolLiteral = pure LBoolLiteral
+label symbols (Conditional (t1, t2, t3)) =
+  do
+    v1 <- newVar
+    v2 <- newVar
+    v3 <- newVar
+    t1' <- label symbols t1
+    t2' <- label symbols t2
+    t3' <- label symbols t3
+    return $ LConditional ((v1, t1'), (v2, t2'), (v3, t3'))
+label symbols (Lambda (x, t)) =
+  do
+    var1 <- newVar
+    var2 <- newVar
+    let symbols' = Map.insert x var1 symbols
+    t' <- label symbols' t
+    return $ LLambda (var1, (var2, t'))
+label symbols (Variable name) =
+  case Map.lookup name symbols of
+    Just var -> pure $ LVariable var
+    Nothing -> pure LUndefined
+label symbols (Application (t1, t2)) =
+  do
+    t1' <- label symbols t1
+    t2' <- label symbols t2
+    v1 <- newVar
+    v2 <- newVar
+    return $ LApplication ((v1, t1'), (v2, t2'))
+
+infer' :: LVar -> LTerm -> [(LVar, Type)]
+infer' var LIntLiteral =
+  [(var, Int)]
+infer' var LBoolLiteral =
+  [(var, Bool)]
+infer' var (LConditional ((v1, t1), (v2, t2), (v3, t3))) =
+  [(v1, Bool), (v2, TVar v3), (var, TVar v2)]
+    ++ infer' v1 t1
+    ++ infer' v2 t2
+    ++ infer' v3 t3
+infer' var (LLambda (v1, (v2, t))) =
+  infer' v2 t
+    ++ [(var, Function (TVar v1, TVar v2))]
+infer' var (LVariable v1) =
+  [(var, TVar v1)]
+infer' var (LApplication ((v1, t1), (v2, t2))) =
+  [(v1, Function (TVar v2, TVar var))]
+    ++ infer' v1 t1
+    ++ infer' v2 t2
+infer' var LUndefined =
+  [(var, Undefined)]
+
+unify' :: Type -> Type -> Map LVar Type -> Either String (Map LVar Type)
+unify' t1 t2 subst | t1 == t2 = Right subst
+unify' Undefined _ _ = Left "Undefined"
+unify' _ Undefined _ = Left "Undefined"
+unify' (TVar v) t subst =
+  let res1 = Map.lookup v subst
+      res2 =
+        case t of
+          (TVar v') -> Map.lookup v' subst
+          _ -> Nothing
+   in case (res1, res2) of
+        (Just t', _) -> unify' t' t subst
+        (_, Just t') -> unify' (TVar v) t' subst
+        -- TODO: Add the occurs check
+        _ -> Right $ Map.insert v t subst
+unify' t (TVar v) subst = unify' (TVar v) t subst
+unify' (Function (in1, out1)) (Function (in2, out2)) subst =
+  unify' in1 in2 subst >>= unify' out1 out2
+unify' t1 t2 _subst = Left (printf "Could not unify %s with %s" (show t1) (show t2))
+
+-- Map.lookup v >>=
+-- Map.lookup v of
+--   Just t' -> solve t' t subst
+-- TODO: Add occurs check
 
 check :: Term -> (Type, AppState)
 check term =
--- tests/Main.hs
+++ tests/Main.hs
@@ -7,28 +7,3 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "Literals" $
-    do
-      let ctx = Map.empty
-      it "should check bool literals" $
-        infer ctx BoolLiteral `shouldBe` Just Bool
-      it "should check int literals" $
-        infer ctx IntLiteral `shouldBe` Just Int
-
-  describe "Conditionals" $
-    do
-      let ctx = Map.empty
-      it "should accept a correct conditional" $
-        infer ctx (Conditional (BoolLiteral, IntLiteral, IntLiteral)) `shouldBe` Just Int
-      it "should reject conditional with non-bool condition" $
-        infer ctx (Conditional (IntLiteral, IntLiteral, IntLiteral)) `shouldBe` Nothing
-
-  describe "Functions" $
-    do
-      let negation = Lambda ((Bool, Bool), "x", Conditional (Variable "x", BoolLiteral, BoolLiteral))
-      let toBinary = Lambda ((Bool, Int), "x", Conditional (Variable "x", IntLiteral, IntLiteral))
-      do
-        let ctx = Map.empty
-        it "should typecheck x -> if x then 1 else 0" $
-          infer ctx toBinary `shouldBe` Just (Function (Bool, Int))
-        it "should typecheck x -> if x then false else true" $
-          infer ctx negation `shouldBe` Just (Function (Bool, Bool))
